8a1c7c7bc64f82698bdca544f51e01c5e4a77d14
akTARDIGRADE13 <56926258+akTARDIGRADE13@users.noreply.github.com>
2026-09-03 07:03:30 +0900

    refactor: split documentation into multiple pages

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,22 @@ export default defineConfig({
       input: {
         home: resolve(import.meta.dirname, "index.html"),
         game: resolve(import.meta.dirname, "game/index.html"),
+
         docs: resolve(import.meta.dirname, "docs/index.html"),
+        docsEnvironment: resolve(
+          import.meta.dirname,
+          "docs/environment/index.html",
+        ),
+        docsProject: resolve(import.meta.dirname, "docs/project/index.html"),
+        docsWeb: resolve(import.meta.dirname, "docs/web/index.html"),
+        docsP5js: resolve(import.meta.dirname, "docs/p5js/index.html"),
+        docsSampleGame: resolve(
+          import.meta.dirname,
+          "docs/sample-game/index.html",
+        ),
+        docsGit: resolve(import.meta.dirname, "docs/git/index.html"),
+        docsDeploy: resolve(import.meta.dirname, "docs/deploy/index.html"),
+        docsAdvanced: resolve(import.meta.dirname, "docs/advanced/index.html"),
       },
     },
   },
